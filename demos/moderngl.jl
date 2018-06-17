f247f856f4393361362fe288f36ea1269aa3786a
Jesse Johnson <holocronweaver@gmail.com>
2018-06-16 20:29:55 -0700

    Split pawn definitions to their own file.

--- demos/moderngl.jl
+++ demos/moderngl.jl
@@ -24,50 +24,25 @@ exitApp = false
 
 global nextUpdateQuantaMultiple = 0
 
-function generatePyramid()
-    const vertices = GLfloat[
-        0.0  0.5  0.0
-        0.5 -0.5  0.0
-        -0.5 -0.5  0.0
-        0.0  0.0  1.0
-    ]'
-    const indices = GLint[
-        2, 1, 0,
-        3, 0, 1,
-        2, 0, 3,
-        3, 1, 2,
-    ]
+include("pawns.jl")
 
-    const shadersDir = "shaders"
-    pyramid = Game.Pawn(
-        Renderer.Item(
-            Renderer.Shader(
-                joinpath(shadersDir, "vert.glsl"),
-                joinpath(shadersDir, "frag.glsl")
-            ),
-            Renderer.Mesh(vertices, indices)
-        )
-    )
-    pyramid
+function handleKey(key, scancode, action)
+    name = GLFW.GetKeyName(key, scancode)
+    if name == nothing
+        println("scancode $scancode ", action)
+    else
+        println("key $name ", action)
+    end
+    # if key != nothing
+    if key == GLFW.KEY_SPACE
+        global exitApp = true
+    end
 end
 
 function modernGLDemo()
     window = Renderer.createWindow("ModernGL Example")
     Renderer.init()
 
-    # Setup input key handler.
-    function handleKey(key, scancode, action)
-        name = GLFW.GetKeyName(key, scancode)
-        if name == nothing
-            println("scancode $scancode ", action)
-        else
-            println("key $name ", action)
-        end
-        # if key != nothing
-        if key == GLFW.KEY_SPACE
-            Renderer.exit()
-        end
-    end
     GLFW.SetKeyCallback(
         window,
         (_, key, scancode, action, mods) -> handleKey(key, scancode, action)
@@ -107,16 +82,9 @@ function modernGLDemo()
     GLFW.Terminate()
 end
 
-function exit()
-    global exitApp = true
-end
-
 function simulate(pawns, secsSinceUpdate)
     tic()
     while secsSinceUpdate > chronon
-        # Pulse the background blue.
-        # glClearColor(0.0, 0.0, 0.5 * (1 + sin(i * 0.02)), 1.0)
-
         # Threads.@threads for pawn in pawns
         for pawn in pawns
             #TODO: generic update
@@ -132,24 +100,6 @@ function simulate(pawns, secsSinceUpdate)
     end
     secsSinceUpdate + toq()
 end
-
-function updatePyramid(pyramid::Game.Pawn, secsSinceUpdate)
-    # Move the triangle and color.
-    pyramid.pos = GLfloat[
-        0,
-        0,
-        # sin(secsSinceUpdate) + 0.1 * sin(secsSinceUpdate / 2)
-        # sin(secsSinceUpdate / 50) + 0.2 * cos(secsSinceUpdate / 5)
-        0
-    ]
-    pyramid.scale = GLfloat[1, 1, 1]
-    pyramid.orientation = qrotation([0, 1, 0], 5.0 * secsSinceUpdate) * pyramid.orientation
-    pyramid.rot = eye(GLfloat, 4)
-    pyramid.rot[1:3, 1:3] = rotationmatrix(pyramid.orientation)
-
-    # pyramid.color = GLfloat[0, 1, sin(i / 50), 1]
-end
-
 
 if profile
     Profile.init(delay=0.01)
---
+++ demos/pawns.jl
@@ -0,0 +1,46 @@
+function generatePyramid()
+    const vertices = GLfloat[
+        0.0  0.5  0.0
+        0.5 -0.5  0.0
+        -0.5 -0.5  0.0
+        0.0  0.0  1.0
+    ]'
+    const indices = GLint[
+        2, 1, 0,
+        3, 0, 1,
+        2, 0, 3,
+        3, 1, 2,
+    ]
+
+    const shadersDir = "shaders"
+    pyramid = Game.Pawn(
+        Renderer.Item(
+            Renderer.Shader(
+                joinpath(shadersDir, "vert.glsl"),
+                joinpath(shadersDir, "frag.glsl")
+            ),
+            Renderer.Mesh(vertices, indices)
+        )
+    )
+    pyramid
+end
+
+function updatePyramid(pyramid::Game.Pawn, secsSinceUpdate)
+    # Move the triangle and color.
+    pyramid.pos = GLfloat[
+        0,
+        0,
+        # sin(secsSinceUpdate) + 0.1 * sin(secsSinceUpdate / 2)
+        # sin(secsSinceUpdate / 50) + 0.2 * cos(secsSinceUpdate / 5)
+        0
+    ]
+    pyramid.scale = GLfloat[1, 1, 1]
+    pyramid.orientation = qrotation([0, 1, 0], 5.0 * secsSinceUpdate) * pyramid.orientation
+    pyramid.rot = eye(GLfloat, 4)
+    pyramid.rot[1:3, 1:3] = rotationmatrix(pyramid.orientation)
+
+    # pyramid.color = GLfloat[0, 1, sin(i / 50), 1]
+
+    # Pulse the background blue.
+    # glClearColor(0.0, 0.0, 0.5 * (1 + sin(i * 0.02)), 1.0)
+end
